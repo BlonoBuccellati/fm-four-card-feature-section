286e03993f7859d9566cc1ead2c708c71f7c17a0
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-04-14 22:41:09 +0900

    chore:準備（デザインシステム）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,9 +12,16 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["200", "400", "600"],
+});
+
 export const metadata: Metadata = {
   title: "Frontend Mentor | Four card feature section",
   description: "Four card feature section of Frontend Mentor Solution",
+  icons: "favicon-32x32.png",
 };
 
 export default function RootLayout({
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div></div>;
+  return <div className="lg:text-2xl">test</div>;
 }
