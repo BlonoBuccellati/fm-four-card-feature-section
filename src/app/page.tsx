71aa804a17a1f13766cfb99908f34e99bae1d67f
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-04-22 21:41:05 +0900

    refactor:モジュール化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import CardHero from "@/components/header/card-hero";
 
 export default function Home() {
   return (
-    <div className="font-poppins text-neutral-gray-500 min-h-screen bg-[#FAFAFA] font-thin">
+    <main className="font-poppins text-neutral-gray-500 min-h-screen bg-[#FAFAFA] font-thin">
       <div className="m-auto flex w-[86%] flex-col gap-15 pt-[clamp(60px,10.5vw,102px)] pb-[clamp(2.5rem,.09rem+10.25vw,5rem)] md:gap-[74px]">
         {/* ヒーローセクション？ */}
         <CardHero />
         {/* コンテンツ */}
         <CardContent />
       </div>
-    </div>
+    </main>
   );
 }
