a822fc0bc114d3e4c9a2404a1a84f2e4606a1e3e
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-04-22 21:46:24 +0900

    fix(style): 微調整

--- src/components/contents/card.tsx
+++ src/components/contents/card.tsx
@@ -18,7 +18,7 @@ const Card = ({
       className={`mx-auto grid max-w-[314px] gap-8 rounded border-t-4 bg-white p-8 shadow-xl lg:max-w-[350px] ${borderColor}`}
     >
       <div>
-        <h2 className="text-preset-3 font-semibold">{title}</h2>
+        <h2 className="text-preset-3 mb-2 font-semibold">{title}</h2>
         <p className="text-preset-5 font-normal">{children}</p>
       </div>
       <div className="mb-4.5 flex h-[64px] flex-row-reverse">
